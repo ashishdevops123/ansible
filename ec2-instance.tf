c2b275eb3088f560b284aea44cca5136df99498e
ashishraj <ashishsmart2401>
2022-09-26 20:25:12 +0530

    added

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -41,16 +41,20 @@ provisioner "remote-exec" {
         inline = [
           "sudo apt update",
           "sudo apt install python3  -y",
-          "sudo apt install git  -y"
+         
         ]
     }
+provisioner "local-exec" {
+        command = "cd ~ "
+      
+    }    
 provisioner "local-exec" {
         command = "git clone https://github.com/ashishdevops123/playbooks.git"
       
     }     
 
 provisioner "local-exec" {
-        command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i '${aws_instance.ubuntu.public_ip},' --private-key './kubernetes.pem' /home/ansible/playbooks/tomcat9.tomcat.yaml"
+        command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i '${aws_instance.ubuntu[count.index].public_ip},' --private-key './kubernetes.pem' /home/ansible/playbooks/tomcat9.tomcat.yaml"
       
     }    
 
